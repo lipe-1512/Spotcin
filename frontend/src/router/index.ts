46b916f7b8d0c1a0cac610fcab3dd227b05b5ad9
Thiago de Souza Farias <tsf@cin.ufpe.br>
2024-07-21 16:19:50 -0300

    feat: Create menu layout frontend

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import ToDoView from '../views/ToDoView.vue'
+import MainPage from '../views/MainPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,16 +7,8 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: ToDoView
+      component: MainPage
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
   ]
 })
 
