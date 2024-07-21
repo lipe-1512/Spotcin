f22b8115a2954d35944cdf4935a9670b598594e0
Filipe Fernando da Silva Balbino <felipelolgxx@gmail.com>
2024-07-20 22:20:35 -0300

    feat: update src/entities/categoria.entity.ts

--- backend/src/entities/categoria.entity.ts
+++ backend/src/entities/categoria.entity.ts
@@ -2,10 +2,12 @@ import BaseEntity from './base.entity';
 
 class CategoriaEntity extends BaseEntity {
   nome: string;
+  playlists: string[];
 
   constructor(data: Partial<CategoriaEntity>) {
     super(data.id || '');
     this.nome = data.nome || '';
+    this.playlists = data.playlists || [];
   }
 }
 
